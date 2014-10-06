bad9b60c6ca39fbea2e461ac22f51761a46bc4ee
41378 <41378@PC003437.coll.lr.local>
2014-10-06 10:23:28 +0100

    completed Selection class exercise 1 - 5

--- Revision_4 Sel_Class_Ex.py
+++ Revision_4 Sel_Class_Ex.py
@@ -4,11 +4,12 @@
 
 number1 = int(input("Please enter a number: "))
 number2 = int(input("Please enter a different number: "))
-numver3 = int(input("Please enter a third number: ")
+number3 = int(input("Please enter a third number: "))
               
 if number1 > number2 and number2 > number3:
     print("{0} is greater than {1}".format(number1, number2))
-elif  
+elif number3 > number1 and number2 < number1:
+    print("{0} is greater than {1}".format(number3, number1))
 elif number1 == number2:
     print("These numbers are the same")
 else:
